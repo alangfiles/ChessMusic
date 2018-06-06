2622bc3d43461b47ab77911ad556ccbd1f86c928
Alan Files <alangfiles@gmailcom>
2018-06-06 00:03:32 -0400

    adding move display, and castling workaround

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ const processedInput = ["e4", "e5", "Nf3", "Nc6", "Bb5", "a6", "Ba4", "Nf6", "O-
 const simplified = ["Pe4", "Pe5", "Nf3", "Nc6", "Bb5", "Pa6", "Ba4", "Nf6", "O-O", "Be7", "Re1", "Pb5", "Bb3", "Pd6", "Pc3", "O-O", "Ph3", "Nb8", "Pd4", "Nbd7", "Pc4", "Pc6", "Pb5", "Pb5", "Nc3", "Bb7", "Bg5", "Pb4", "Nb1", "Ph6", "Bh4", "Pc5", "Pe5", "Ne4", "Be7", "Qe7", "Pd6", "Qf6", "Nd2", "Nd6", "Nc4", "Nc4", "Bc4", "Nb6", "Ne5", "Re8", "Bf7", "Rf7", "Nf7", "Re1", "Qe1", "Kf7", "Qe3", "Qg5", "Qg5", "Pg5", "Pb3", "Ke6", "Pa3", "Kd6", "Pb4", "Pb4", "Ra5", "Nd5", "Pf3", "Bc8", "Kf2", "Bf5", "Ra7", "g6", "Ra6", "Kc5", "Ke1", "Nf4", "Pg3", "Nh3", "Kd2", "Kb5", "Rd6", "Kc5", "Ra6", "Nf2", "Pg4", "Bd3", "Re6"];
 
 
-const split = [["P", "e", 4], ["P", "e", 5], ["N", "f", 3], ["N", "c", 6], ["B", "b", 5], ["P", "a", 6], ["B", "a", 4], ["N", "f", 6],   /*["O", "O", 0],*/["B", "e", 7], ["R", "e", 1], ["P", "b", 5], ["B", "b", 3], ["P", "d", 6], ["P", "c", 3],   /*["O", "O", 0],*/["P", "h", 3], ["N", "b", 8], ["P", "d", 4], ["N", "d", 7], ["P", "c", 4], ["P", "c", 6], ["P", "b", 5], ["P", "b", 5], ["N", "c", 3], ["B", "b", 7], ["B", "g", 5], ["P", "b", 4], ["N", "b", 1], ["P", "h", 6], ["B", "h", 4], ["P", "c", 5], ["P", "e", 5], ["N", "e", 4], ["B", "e", 7], ["Q", "e", 7], ["P", "d", 6], ["Q", "f", 6], ["N", "d", 2], ["N", "d", 6], ["N", "c", 4], ["N", "c", 4], ["B", "c", 4], ["N", "b", 6], ["N", "e", 5], ["R", "e", 8], ["B", "f", 7], ["R", "f", 7], ["N", "f", 7], ["R", "e", 1], ["Q", "e", 1], ["K", "f", 7], ["Q", "e", 3], ["Q", "g", 5], ["Q", "g", 5], ["P", "g", 5], ["P", "b", 3], ["K", "e", 6], ["P", "a", 3], ["K", "d", 6], ["P", "b", 4], ["P", "b", 4], ["R", "a", 5], ["N", "d", 5], ["P", "f", 3], ["B", "c", 8], ["k", "f", 2], ["B", "f", 5], ["R", "a", 7], ["P", "g", 6], ["B", "e", 6], ["B", "e", 5], ["B", "e", 1], ["B", "e", 4], ["B", "e", 3], ["B", "e", 3], ["B", "e", 2], ["B", "e", 5], ["B", "e", 6], ["B", "e", 5], ["R", "a", 6], ["N", "f", 2], ["P", "g", 4], ["B", "d", 3], ["R", "e", 6]];
+const split = [["P", "e", 4], ["P", "e", 5], ["N", "f", 3], ["N", "c", 6], ["B", "b", 5], ["P", "a", 6], ["B", "a", 4], ["N", "f", 6], ["O", "O", 0], ["B", "e", 7], ["R", "e", 1], ["P", "b", 5], ["B", "b", 3], ["P", "d", 6], ["P", "c", 3], ["O", "O", 0], ["P", "h", 3], ["N", "b", 8], ["P", "d", 4], ["N", "d", 7], ["P", "c", 4], ["P", "c", 6], ["P", "b", 5], ["P", "b", 5], ["N", "c", 3], ["B", "b", 7], ["B", "g", 5], ["P", "b", 4], ["N", "b", 1], ["P", "h", 6], ["B", "h", 4], ["P", "c", 5], ["P", "e", 5], ["N", "e", 4], ["B", "e", 7], ["Q", "e", 7], ["P", "d", 6], ["Q", "f", 6], ["N", "d", 2], ["N", "d", 6], ["N", "c", 4], ["N", "c", 4], ["B", "c", 4], ["N", "b", 6], ["N", "e", 5], ["R", "e", 8], ["B", "f", 7], ["R", "f", 7], ["N", "f", 7], ["R", "e", 1], ["Q", "e", 1], ["K", "f", 7], ["Q", "e", 3], ["Q", "g", 5], ["Q", "g", 5], ["P", "g", 5], ["P", "b", 3], ["K", "e", 6], ["P", "a", 3], ["K", "d", 6], ["P", "b", 4], ["P", "b", 4], ["R", "a", 5], ["N", "d", 5], ["P", "f", 3], ["B", "c", 8], ["K", "f", 2], ["B", "f", 5], ["R", "a", 7], ["P", "g", 6], ["B", "e", 6], ["B", "e", 5], ["B", "e", 1], ["B", "e", 4], ["B", "e", 3], ["B", "e", 3], ["B", "e", 2], ["B", "e", 5], ["B", "e", 6], ["B", "e", 5], ["R", "a", 6], ["N", "f", 2], ["P", "g", 4], ["B", "d", 3], ["R", "e", 6]];
 
 
 const noteLength = 150;
@@ -78,54 +78,70 @@ function playSongFromIndex(index) {
   //Start note
   let current = split[index];
 
-  let duration = durations[current[0]];
-  let note = baseNotes[current[1]] * Math.pow(2, current[2]);
-  note = (note / 32) + 300;
+
+  let duration, note, type;
+
+  if (current[0] =="O"){
+    duration = durations.K;
+    note = baseNotes.a;
+    note = (note / 32) + 300;
+    type = types.K;
+  } else {
+    duration = durations[current[0]];
+    note = baseNotes[current[1]] * Math.pow(2, current[2]);
+    note = (note / 32) + 300;
+    type = types[current[0]];
+  }
+
+
+  displayMove(index);
 
   let oscillator = context.createOscillator(); // Create sound source
 
   oscillator.connect(context.destination); // Connect sound to output
   oscillator.start();
-
-  oscillator.type = types[current[0]];// index%2==0?"triangle":'sine';
+  oscillator.type = type;// index%2==0?"triangle":'sine';
   oscillator.frequency.value = note;
 
   setTimeout(function () {
     oscillator.stop();
     if (index + 1 < split.length)
       playSongFromIndex(index + 1);
-  }, duration/*-100*/);
+  }, duration);
 
 }
 
 function playSound(note, time, duration) {
-  
-  let oscillator = context.createOscillator(); 
-  
+
+  let oscillator = context.createOscillator();
+
   oscillator.type = 'sine';
   oscillator.frequency.value = note;
 
   oscillator.connect(context.destination); // Connect sound to output
   oscillator.start(time);
-  oscillator.stop(time+(duration/1000));
+  oscillator.stop(time + (duration / 1000));
 
 }
-
-playSong();
 
 function playSong(index) {
 
   var startTime = context.currentTime;
   let timeSoFar = 0;
 
-  for(var i =0; i<split.length; i++){
+  for (var i = 0; i < split.length; i++) {
     let current = split[i];
     let duration = durations[current[0]];
     let note = baseNotes[current[1]] * Math.pow(2, current[2]);
     note = (note / 32) + 300; //scale down the note
 
-    playSound(note, startTime+timeSoFar, duration )
-    timeSoFar+=(duration/1000);
+    playSound(note, startTime + timeSoFar, duration);
+    displayMove(current);
+    timeSoFar += (duration / 1000);
   }
 
+}
+
+function displayMove(move) {
+  document.querySelector("#move").innerHTML = processedInput[move];
 }
